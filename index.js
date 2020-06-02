1b3c0fdfc75eeb7735ca601db77703e3171f9659
Fedor Ganin <fedos.sorryguy@gmail.com>
2020-06-02 17:01:36 +0300

    add models

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const { errors, Joi, celebrate } = require('celebrate');
+
+const {
+  PORT, DATABASE_URL, SERVER_PARAMS, URL_REGEX,
+} = require('./config');
 
 const app = express();
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.listen(PORT);
+mongoose.connect(DATABASE_URL, SERVER_PARAMS);
