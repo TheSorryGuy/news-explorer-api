97cfdac1997ea24517928269439a7c43b8f6ac26
Fedor Ganin <fedos.sorryguy@gmail.com>
2020-06-02 22:30:22 +0300

    create user router

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const helmet = require('helmet');
 const { errors, Joi, celebrate } = require('celebrate');
 const auth = require('./middlewares/auth');
 const users = require('./routes/users');
-
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { login, createUser } = require('./controllers/users');
+const errorHandler = require('./middlewares/errorHandler');
 
 const {
-  PORT, DATABASE_URL, SERVER_PARAMS, URL_REGEX,
+  PORT, DATABASE_URL, SERVER_PARAMS,
 } = require('./config');
 
 const app = express();
@@ -21,12 +22,31 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(requestLogger);
 
-app.post('/signup', createUser);
-app.post('/signin', login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8).max(30),
+  }),
+}), createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8).max(30),
+  }),
+}), login);
 
 app.use(auth);
 app.use('/', users);
+
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
+app.use('/', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.listen(PORT);
 mongoose.connect(DATABASE_URL, SERVER_PARAMS);
